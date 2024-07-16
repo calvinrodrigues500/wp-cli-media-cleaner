ba288ce96dcd8f36f2140f9428d503edb94f43db
calvinrodrigues500 <calvinrodrigues500@gmail.com>
2024-07-16 23:24:10 +0530

    Initial version

---
+++ wp-cli-media-cleaner.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name: WP CLI Media Cleaner
+ * Plugin URI: https://calvinrodrigues.in
+ * Description: A CLI based media cleaner for WordPress
+ * Version: 1.0.0
+ * Author: Calvin Rodrigues
+ * Author URI: https://calvinrodrigues.in
+ * Text Domain: wp-cli-media-cleaner
+ * Requires at least: 6.5
+ * Requires PHP: 7.4
+ *
+ * @package wp-cli-media-cleaner
+ */
+
+defined( 'ABSPATH' ) || die;
